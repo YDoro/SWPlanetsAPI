cb347ffd816219793e310e371bac40401b73f90c
yan doro <yan_mvd@hotmail.com>
2021-05-20 00:32:02 -0300

    chore: test config

---
+++ jest-mongodb-config.js
@@ -0,0 +1,13 @@
+//in memory mongodb instance
+module.exports = {
+    mongodbMemoryServerOptions: {
+        instance: {
+            dbName: 'jest'
+        },
+        binary: {
+            version: '4.0.3',
+            skipMD5: true
+        },
+        autoStart: false
+    }
+}
