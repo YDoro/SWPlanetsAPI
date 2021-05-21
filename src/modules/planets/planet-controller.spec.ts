58871b1cf45ccb39023f1e76a25cc53a19f750ed
yan doro <yan_mvd@hotmail.com>
2021-05-21 08:55:40 -0300

    test: ensure PlanetController.list will throw if repository throws and return an 200 with an array if repository succeeds

--- src/modules/planets/planet-controller.spec.ts
+++ src/modules/planets/planet-controller.spec.ts
@@ -5,8 +5,13 @@ import { PlanetRepository } from './planet-repository'
 describe('PlanetController', () => {
 
     class PlanetRepositoryStub implements PlanetRepository {
+
         create(planet: Planet): Promise<PlanetModel> {
-            return new Promise(resolve => resolve({ ...planet, _id: 'any_id' }))
+            return new Promise(resolve => resolve({ ...planet, id: 'any_id' }))
+        }
+
+        list(): Promise<PlanetModel[]> {
+            return new Promise(resolve => resolve([{ ...planet, id: 'any_id' }]))
         }
 
     }
@@ -18,7 +23,7 @@ describe('PlanetController', () => {
 
     const makeSUT = (): SUTTypes => {
         const repository = new PlanetRepositoryStub()
-        const sut = new PlanetController(repository);
+        const sut = new PlanetController(repository)
         return { repository, sut }
     }
 
@@ -27,24 +32,40 @@ describe('PlanetController', () => {
         climate: 'temperate',
         terrain: 'grasslands'
     }
+    describe('create', () => {
+        test('should call repository with the given object and return the an HttpReponse', async () => {
+            const { sut, repository } = makeSUT()
+            const createSpy = jest.spyOn(repository, 'create')
+            const promise = sut.create(planet)
 
-    test('should call repository with the given object and return the an HttpReponse', async () => {
-        const { sut, repository } = makeSUT();
-        const createSpy = jest.spyOn(repository, 'create')
-        const promise = sut.create(planet)
+            expect(createSpy).toBeCalledWith(planet)
+            await expect(promise).resolves.toEqual({ status: 201, body: { ...planet, id: 'any_id' } })
 
-        expect(createSpy).toBeCalledWith(planet)
-        expect(promise).resolves.toEqual({ status: 201, body: { ...planet, _id: 'any_id' } })
+        })
 
+        test('should throw if repository throws', async () => {
+            const { sut, repository } = makeSUT()
+            jest.spyOn(repository, 'create').mockRejectedValueOnce(new Error('mocked error'))
+            const promise = sut.create(planet)
+            await expect(promise).rejects.toThrowError('mocked error')
+        })
     })
 
-    test('should throw if repository throws', async () => {
-        const { sut, repository } = makeSUT();
-        jest.spyOn(repository, 'create').mockRejectedValueOnce(new Error('mocked error'))
+    describe('list', () => {
+        test('should call repository and return a 200 response', async () => {
+            const { sut, repository } = makeSUT()
+            const listSpy = jest.spyOn(repository, 'list')
+            const promise = sut.list()
+            expect(listSpy).toBeCalledWith()
+            await expect(promise).resolves.toEqual({ status: 200, body: [{ ...planet, id: 'any_id' }] })
+        })
 
-        const promise = sut.create(planet)
-
-        expect(promise).rejects.toThrowError('mocked error')
-
+        test('should throw if repository throws', async () => {
+            const { sut, repository } = makeSUT()
+            jest.spyOn(repository, 'list').mockRejectedValueOnce(new Error('mocked error'))
+            const promise = sut.list()
+            await expect(promise).rejects.toThrowError('mocked error')
+        })
     })
+
 })
