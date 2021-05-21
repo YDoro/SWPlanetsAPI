f6f1f501b045e37a81ffbcaf85d15770ea763c2f
yan doro <yan_mvd@hotmail.com>
2021-05-21 20:46:23 -0300

    feat: create getPlanetByName method declaration

--- src/modules/planets/planet-repository.ts
+++ src/modules/planets/planet-repository.ts
@@ -5,5 +5,6 @@ export interface PlanetRepository {
     list(): Promise<Array<PlanetModel>>
     search(query: object): Promise<Array<PlanetModel>>
     deletePlanetById(id: string): Promise<void>;
+    getPlanetByName(name: string): Promise<PlanetModel>
 
 }
