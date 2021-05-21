5ab3805e8769f3f768e7f32b89c2cfa3c60aded2
yan doro <yan_mvd@hotmail.com>
2021-05-21 19:46:59 -0300

    feat: create GetPlanetMovieApparitionService implementation for SWAPI

---
+++ src/services/sw_api_service.ts
@@ -0,0 +1,55 @@
+import axios, { AxiosInstance } from "axios";
+import { GetPlanetMovieApparitionService } from "./protocols/get_planet_movie_apparition_service";
+interface SWAPIPlanet {
+    name: string
+    rotation_period: string
+    orbital_period: string
+    diameter: string
+    climate: string
+    gravity: string
+    terrain: string
+    surface_water: string
+    population: string
+    residents: string[]
+    films: string[]
+    created: string
+    edited: string
+    url: string
+}
+export class SWAPIService implements GetPlanetMovieApparitionService {
+    private readonly http: AxiosInstance
+    constructor() {
+        this.http = axios.create({ baseURL: 'https://swapi.dev' })
+    }
+    async getPlanetApparitionsByName(name: string): Promise<number> {
+
+        let returnedLength = 1
+        let page = 1
+        let movieApparitions = 0
+
+        while (returnedLength > 0) {
+            let returnedplanets = await this.getPlanets(page)
+            returnedLength = returnedplanets.length
+            returnedplanets.forEach((planet) => {
+                if (planet.name.toLowerCase() === name.toLowerCase()) {
+                    console.log(planet.films)
+                    movieApparitions = planet.films.length
+                    returnedLength = 0
+                }
+            })
+            page++
+        }
+        return movieApparitions
+
+    }
+
+    private async getPlanets(page = 1): Promise<Array<SWAPIPlanet>> {
+        try {
+            const response = await this.http.get('/api/planets/?page=' + page, { validateStatus: (status) => status === 200 })
+            return response.data.results
+        } catch (e) {
+            return []
+        }
+    }
+
+}
