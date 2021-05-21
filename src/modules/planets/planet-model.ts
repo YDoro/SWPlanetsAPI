914431e43f0080104ef08d0470c5b3fbba46497e
yan doro <yan_mvd@hotmail.com>
2021-05-21 19:47:41 -0300

    feat: add an optional field to Planet Interface

--- src/modules/planets/planet-model.ts
+++ src/modules/planets/planet-model.ts
@@ -1,10 +1,11 @@
 export interface Planet {
     name: string
     climate: string,
-    terrain: string
+    terrain: string,
+    movieApparitions?: number
 }
 export interface PlanetModel extends Planet {
     id: string
 }
 
-export const PlanetModelSeachableParams = ['name', 'climate', 'terrain', '_id']
+export const PlanetModelSeachableParams = ['name', 'climate', 'terrain', '_id', 'movieApparitions']
