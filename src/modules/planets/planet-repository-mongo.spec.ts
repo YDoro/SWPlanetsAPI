a042919152bbc0b51d31253e28c5a43729b2dd05
yan doro <yan_mvd@hotmail.com>
2021-05-21 08:57:16 -0300

    test: ensure PlanetMongoRespository.list  will return an array or throw

--- src/modules/planets/planet-repository-mongo.spec.ts
+++ src/modules/planets/planet-repository-mongo.spec.ts
@@ -20,17 +20,30 @@ describe('PlanetMongoRepository', () => {
     }
 
     describe('Create', () => {
-
         test('should return the created planet', async () => {
             const promise = sut.create(planet)
             await expect(promise).resolves.toHaveProperty('id')
         })
-
         test('should throw if database is not connected', async () => {
             const promise = sut.create(planet)
             await MongoHelper.disconnect()
             await expect(promise).rejects.toBeInstanceOf(MongoError)
         })
+    })
+    describe('List', () => {
+        test('should return the list of planets', async () => {
+            await (await MongoHelper.getCollection('planets')).deleteMany({})
+            await sut.create(planet)
+            const promise = sut.list()
+            await expect(promise).resolves.toHaveLength(1)
+        })
+        test('should throw on any error', async () => {
+            const promise = sut.list()
+            await MongoHelper.disconnect()
+            await expect(promise).rejects.toBeInstanceOf(Error)
+        })
+
+
     })
 
 })
