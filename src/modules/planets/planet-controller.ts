0811f08f06507cfd3117294b5cfbe5ab309553a9
yan doro <yan_mvd@hotmail.com>
2021-05-21 08:54:50 -0300

    feat: add PlanetController List method

--- src/modules/planets/planet-controller.ts
+++ src/modules/planets/planet-controller.ts
@@ -8,8 +8,14 @@ export class PlanetController {
     constructor(
         private readonly repository: PlanetRepository
     ) { }
+
     async create(planet: Planet): Promise<HttpResponse> {
         const dbPlanet = await this.repository.create(planet)
         return { status: 201, body: dbPlanet }
+    }
+
+    async list(): Promise<HttpResponse> {
+        const planets = await this.repository.list()
+        return { status: 200, body: planets }
     }
 }
