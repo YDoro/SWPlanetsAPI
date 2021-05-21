5c5d619dbdaeb8f0da7d98199a27e078b3427f0d
yan doro <yan_mvd@hotmail.com>
2021-05-21 08:54:10 -0300

    feat: create list method in PlanetRepository

--- src/modules/planets/planet-repository.ts
+++ src/modules/planets/planet-repository.ts
@@ -2,4 +2,5 @@ import { Planet, PlanetModel } from "./planet-model"
 
 export interface PlanetRepository {
     create(planet: Planet): Promise<PlanetModel>
+    list(): Promise<Array<PlanetModel>>
 }
